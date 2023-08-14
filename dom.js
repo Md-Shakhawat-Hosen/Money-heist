f5c66b11e1277bdcf16d99826571e95b09fbbebd
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-08-15 00:38:45 +0600

    added js file

---
+++ dom.js
@@ -0,0 +1,15 @@
+document.getElementById("submit-btn").addEventListener('click',function(){
+    const loginInputEmail = document.getElementById('login-input-email');
+    const loginInputPassword = document.getElementById("login-input-password");
+ 
+    const loginInputEmailValue = loginInputEmail.value;
+    const loginInputPasswordValue = loginInputPassword.value
+
+    if (loginInputEmailValue === 'abc@gmail.com' && loginInputPasswordValue === 'abc') {
+        window.location.href = './heist.html';
+    }
+    else {
+        alert('Invalid email and password')
+    }
+
+})

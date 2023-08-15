04d77b3d6850e349539fa100eb153ecd54c41caf
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-08-15 12:34:32 +0600

    intractive with button

--- dom.js
+++ dom.js
@@ -1,15 +1,62 @@
-document.getElementById("submit-btn").addEventListener('click',function(){
-    const loginInputEmail = document.getElementById('login-input-email');
-    const loginInputPassword = document.getElementById("login-input-password");
- 
-    const loginInputEmailValue = loginInputEmail.value;
-    const loginInputPasswordValue = loginInputPassword.value
+let previous = 0;
+function add(value) {
 
-    if (loginInputEmailValue === 'abc@gmail.com' && loginInputPasswordValue === 'abc') {
-        window.location.href = './heist.html';
+    const depoTk = document.getElementById("depo-tk");
+    const balanceTk = document.getElementById("balance");
+
+    if (isNaN(value)) {
+      alert("Please input valid number");
+    } else {
+      previous += value;
+      
+      depoTk.innerText = previous;
+      balanceTk.innerText = previous;
+
+    }
+}
+
+   let preWithdraw = 0;
+ function subtractWithdraw(value) {
+    const balanceInput = document.getElementById('balance');
+    let balanceValue = parseFloat(balanceInput.innerText)
+    
+
+    const withdrawBalance = document.getElementById("withdraw-balance");
+    if (isNaN(value)) {
+        alert("Please valid number");
+    }
+    else if (value > balanceValue) {
+        alert("you can't withdraw, because your balance is insufficient")
     }
     else {
-        alert('Invalid email and password')
+         
+        balanceValue = balanceValue - value
+        preWithdraw += value;
+        withdrawBalance.innerText = preWithdraw;
+        balanceInput.innerText = balanceValue;
     }
+ }
+// Deposit section
 
-})
+document.getElementById("depositId").addEventListener("click", function () {
+
+  const depositInput = document.getElementById("deposit-input");
+  const depositInputValue = parseFloat(depositInput.value);
+   add(depositInputValue);
+
+   depositInput.value = "";
+   
+});
+
+
+
+
+// withdraw
+
+document.getElementById("withdrawId").addEventListener("click", function () {
+  const withdrawInput = document.getElementById("withdrawInput");
+  const withdrawInputValue = parseFloat(withdrawInput.value);
+  subtractWithdraw(withdrawInputValue);
+
+  withdrawInput.value = "";
+});

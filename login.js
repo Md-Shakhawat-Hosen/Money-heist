be0fcbff7b99bfe01281a8001569e90b4f5356b0
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-08-15 16:06:39 +0600

    added eye slash for password fill

--- login.js
+++ login.js
@@ -14,3 +14,24 @@ document.getElementById("submit-btn").addEventListener("click", function () {
     alert("Invalid email and password");
   }
 });
+
+document.getElementById("eye").addEventListener("click", function () {
+  const eye = document.getElementById("eye-bondo");
+
+  const inputVal = document.getElementById("login-input-password");
+
+  if (eye.classList[1] === "fa-eye-slash") {
+    eye.removeAttribute("class");
+
+    eye.setAttribute("class", "fa-solid fa-eye");
+
+    inputVal.removeAttribute("type");
+    inputVal.setAttribute("type", "text");
+  } else {
+    eye.removeAttribute("class");
+    eye.setAttribute("class", "fa-solid fa-eye-slash");
+
+    inputVal.removeAttribute("type");
+    inputVal.setAttribute("type", "password");
+  }
+});
